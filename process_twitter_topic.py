42c73304bb210fc0b16923fe0a726c4ad796978e
Keith Dimatteo <kdimat@boskdima24048.wellmanage.com>
2013-01-28 11:44:22 -0500

    entity scrubbing, return JSON by default, debug flag for console testing

--- process_twitter_topic.py
+++ process_twitter_topic.py
@@ -14,21 +14,27 @@ API_KEY =  os.environ['API_KEY']
 calais = Calais(API_KEY, submitter="python-calais demo")
 
 
-def getTweets(hash_tag):
+def getTweets(term, return_json=True, debug=False):
     '''
     Search Twitter for the provided hast_tag to generate a bag of words.
     Return entities and topics from OpenCalais
     '''
-    #print urllib.quote(hash_tag)
-    u = 'http://search.twitter.com/search.json?lang=en&rpp=100&result_type=recent&q=%s' % urllib.quote_plus(hash_tag)
+    
+    if debug is True:
+        print "Sanitized Search term: %s " % urllib.quote(term)
+    
+    u = 'http://search.twitter.com/search.json?lang=en&rpp=100&result_type=recent&q=%s' % urllib.quote_plus(term)
     result = urllib2.urlopen(u)
     o = simplejson.loads(result.read())
     content = ""
     L = []
     for tweet in o['results']:
         L.append(tweet['text'])
-    
-    OUTPUT = []
+
+    entities = [] 
+    topics = []
+    relations = []
+
     pattern = '((mailto\:|(news|(ht|f)tp(s?))\://){1}\S+)'
 
     content = " ".join(L)
@@ -37,35 +43,57 @@ def getTweets(hash_tag):
     try:
         result = calais.analyze(content)
         
-        '''
-        print "--------"
-        result.print_topics()
-        print "--------"
-        result.print_entities()
-        print "--------"
-        result.print_relations()
-        '''
+        if debug is True:
+            print "--------\nTopics:\n--------------------"
+            result.print_topics()
+            print "--------\nEntities:\n--------------------"
+            result.print_entities()
+            print "--------\nRelations:\n--------------------"
+            result.print_relations()
+
     except:
         print "something is broken"
         pass
 
     try:
         for item in result.entities:
-                if not re.match(pattern, item['name']):
-                    OUTPUT.append(item['name'])
+            if not re.match(pattern, item['name']) and ("RT" not in item["name"]):
+                entities.append(item['name'])
     except:
-        print "no entities found"
+        pass
+        #print "no entities found"
                 
     try:
         for item in result.topics:
-            if not re.match(pattern, item['categoryName']):
-                OUTPUT.append(item['categoryName'])
+            topics.append(item['categoryName'])
     except:
-        print "no topics found"
+        pass
+        #print "no topics found"
 
-    return OUTPUT 
+    '''
+
+    # @todo need additional logic as key names are variable:
+    # e.g.: 'careertype', 'instances', 'company', 'person', 'position', '__reference'
+    try:
+        for item in result.relations:
+            for k, v in item:
+                relations.append(item['relation'])
+    except:
+        pass
+    '''
+
+    o = {"calais_results":{"entities":entities, "topics":topics, "relations":relations}}
+
+    if(return_json is True):
+        for d in result.relations:
+            print d.keys()
+        return simplejson.dumps(o);
+    else :
+        return o
+
 
 if __name__ == "__main__":
     var = sys.argv[1]
-    o = getTweets(var)
-    print ", ".join(o)
+    o = getTweets(var, True, False)
+    print o
+    #print ", ".join(o)
